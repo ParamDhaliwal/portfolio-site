416dfb505d94a8115bd7927fee582798bbad9661
TacklessNebula3 <veerdhaliwal22@gmail.com>
2020-08-28 17:11:21 -0700

    andded routing and different links for particular pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage.js';
+import ContactPage from './pages/ContactPage';
 
 class App extends React.Component {
 
@@ -48,6 +51,9 @@ class App extends React.Component {
           </Navbar.Collapse>
         </Navbar>
 
+        <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} /> } />
+        <Route path="/about" exact render={() => <AboutPage title={this.state.about.title} /> } />
+        <Route path="/contact" exact render={() => <ContactPage title={this.state.contact.title} /> } />
         <Footer />
 
       </Container>
